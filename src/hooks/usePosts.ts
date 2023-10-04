1d73584c3e951d74e27365389944c80e0c087cc8
Ali Akbari <7731.aa@gmail.com>
2023-10-04 11:12:34 +0330

    Create hook for get the list of post with infinite query

---
+++ src/hooks/usePosts.ts
@@ -0,0 +1,29 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import axios from "axios";
+import Post from "../components/PostList/Post.interface";
+
+interface PostQuery {
+	pageSize: 10;
+}
+
+const usePosts = (query: PostQuery) =>
+	useInfiniteQuery({
+		queryKey: ["posts", query],
+		queryFn: ({ pageParam }) =>
+			axios
+				.get<Post[]>("https://jsonplaceholder.typicode.com/posts", {
+					params: {
+						_start: (pageParam - 1) * query.pageSize,
+						_limit: query.pageSize,
+					},
+				})
+				.then((res) => res.data)
+				.catch((err) => err.message),
+		keepPreviousData: true,
+		staleTime: 1 * 60 * 1000,
+		getNextPageParam: (lastPage, allPages) => {
+			return lastPage.length > 1 ? allPages.length + 1 : undefined;
+		},
+	});
+
+export default usePosts;
