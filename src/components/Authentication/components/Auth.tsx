567d83df832ddeedbfb852bd24a108d369b37724
Ali Akbari <7731.aa@gmail.com>
2023-10-22 09:44:25 +0330

    ref: refactror auth.tsx file

--- src/components/Authentication/components/Auth.tsx
+++ src/components/Authentication/components/Auth.tsx
@@ -4,54 +4,54 @@ import AuthContext from "../contexts/AuthContext";
 import useAuth from "../hooks/useAuth";
 
 const Login = () => {
-  const userNameRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
+	const userNameRef = useRef<HTMLInputElement>(null);
+	const passwordRef = useRef<HTMLInputElement>(null);
 
-  const { user, dispatch } = useAuth();
+	const { user, dispatch } = useAuth();
 
-  if (!user.userName)
-    return (
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          if (userNameRef.current && passwordRef.current) {
-            const user = {
-              userName: userNameRef.current.value,
-              password: passwordRef.current.value,
-            };
-            console.log(user);
-            dispatch({ type: "LOGIN", user });
-          }
-        }}
-      >
-        <div>
-          <label htmlFor="">User Name</label>
-          <input ref={userNameRef} type="text" className="form-control" />
-        </div>
-        <div>
-          <label htmlFor="">Password</label>
-          <input ref={passwordRef} type="password" className="form-control" />
-        </div>
-        <button type="submit" className="btn btn-primary mt-2">
-          Login
-        </button>
-      </form>
-    );
+	if (!user.userName)
+		return (
+			<form
+				onSubmit={(event) => {
+					event.preventDefault();
+					if (userNameRef.current && passwordRef.current) {
+						const user = {
+							userName: userNameRef.current.value,
+							password: passwordRef.current.value,
+						};
+						console.log(user);
+						dispatch({ type: "LOGIN", user });
+					}
+				}}
+			>
+				<div>
+					<label htmlFor="">User Name</label>
+					<input ref={userNameRef} type="text" className="form-control" />
+				</div>
+				<div>
+					<label htmlFor="">Password</label>
+					<input ref={passwordRef} type="password" className="form-control" />
+				</div>
+				<button type="submit" className="btn btn-primary mt-2">
+					Login
+				</button>
+			</form>
+		);
 
-  if (user.userName)
-    return (
-      <>
-        <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <h5>User: {user.userName}</h5>
-          <button
-            className="btn btn-outline-danger"
-            onClick={() => dispatch({ type: "LOGOUT" })}
-          >
-            Logout
-          </button>
-        </div>
-      </>
-    );
+	if (user.userName)
+		return (
+			<>
+				<div style={{ display: "flex", justifyContent: "space-between" }}>
+					<h5>User: {user.userName}</h5>
+					<button
+						className="btn btn-outline-danger"
+						onClick={() => dispatch({ type: "LOGOUT" })}
+					>
+						Logout
+					</button>
+				</div>
+			</>
+		);
 };
 
 export default Login;
